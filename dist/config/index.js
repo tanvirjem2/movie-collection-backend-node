ab87d3297b78daa41be34f0df0c47329a335ab24
tanvirjem2 <tanviranjum046@gmail.com>
2024-06-23 17:47:02 +0600

    Add ESLint configuration

--- dist/config/index.js
+++ dist/config/index.js
@@ -1,11 +1,12 @@
 "use strict";
+// ---- import dotenv ----
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-app.get('/', (req, res) => {
-    res.send('Tanvir Anjum Shozib');
-});
-exports.default = app;
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+exports.default = {
+    port: process.env.PORT,
+    db_url: process.env.DB_URL
+};
--- src/server.ts
+++ src/server.ts
@@ -9,8 +9,6 @@ async function main() {
     try {
         await mongoose.connect(config.db_url as string);
 
-        const port = 5000;
-
         app.listen(config.port, () => {
             console.log(`Example app listening on port ${config.port}`)
         })
